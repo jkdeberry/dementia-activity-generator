3135231f33c03c07db15360b4b5f4e8ff094852c
jkdeberry <debejmac@gmail.com>
2025-01-02 16:41:14 -0500

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-function generateActivity(event) {
-    event.preventDefault();
+  // Display the generated activity
 
-    new Typewriter("#activity", {
-      strings: ["Work a puzzle"],
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+function displayActivity(response) {
+  console.log("Activity generated");
+  new Typewriter("#activity", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+
+function generateActivity(event) {
+  event.preventDefault();
+  
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "4e2df5aotaa983694533f2b4440ef095";
+    let prompt = `User instructions: Generate an activity for a person with dementia involving ${instructionsInput.value}`;
+    let context = "You are a Dementia activity expert and love to assist the elderly with exercises indoors and outside. Your mission is to find different exercises for them to perform that are very simple with short text in basic html. Make sure to follow the user instructions below. Sign the activity with `SheCodes AI` inside a <strong> element";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
+    // Make a call to the API
+
+    console.log("Generating activity");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiURL).then(displayActivity);
+
+  
+
+    
 }
 
 let activityFormElement = document.querySelector("#activity-generator-form")
