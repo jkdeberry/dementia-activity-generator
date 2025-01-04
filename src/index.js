03e1cb31582e0d72dc2c9313af3c870886b40896
jkdeberry <debejmac@gmail.com>
2025-01-03 21:41:33 -0500

    Removed the console.logs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
   // Display the generated activity
 
-function displayActivity(response) {
-  console.log("Activity generated");
-  new Typewriter("#activity", {
-    strings: response.data.answer,
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
-}
-
+  function displayActivity(response) {
+    
+    new Typewriter("#activity", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
 
 function generateActivity(event) {
   event.preventDefault();
@@ -18,23 +17,14 @@ function generateActivity(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "4e2df5aotaa983694533f2b4440ef095";
     let prompt = `User instructions: Generate an activity for a person with dementia involving ${instructionsInput.value}`;
-    let context = "You are a Dementia activity expert and love to assist the elderly with exercises indoors and outside. Your mission is to find different exercises for them to perform that are very simple with short text in basic html. Make sure to follow the user instructions below. Sign the activity with `SheCodes AI` inside a <strong> element";
+    let context = "You are a Dementia activity expert and love to assist the elderly with exercises indoors and outside. Your mission is to find different exercises for them to perform that are very simple with short text in basic html. Make sure to follow the user instructions below. Sign the activity with <strong>SheCodes AI</strong> inside a <strong> element";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let activityElement = document.querySelector("#activity");
     activityElement.classList.remove("hidden");
     activityElement.innerHTML = `<div class="generating">⏳Generating a dementia activity related to ${instructionsInput.value}</div>`
-    
 
-    console.log("Generating activity");
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
-
-    axios.get(apiURL).then(displayActivity);
-
-  
-
-    
+    axios.get(apiURL).then(displayActivity);    
 }
 
 let activityFormElement = document.querySelector("#activity-generator-form")
